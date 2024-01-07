d765e4faf09691924e9e30be6ca425038aa88238
pulkit-singhall <singhal.pulkit667@gmail.com>
2024-01-07 23:31:09 +0530

    css

---
+++ script.js
@@ -0,0 +1,32 @@
+// const change_theme_btn = document.getElementById('change_theme');
+
+// const body = document.getElementById('main_body');
+
+// let whiteTheme = true;
+
+// console.log(change_theme_btn.className);
+
+// let age = prompt("Tell your age?");
+
+// console.log(age);
+
+// change_theme_btn.addEventListener(
+//     'click', function (event) {
+//         console.log(event.shiftKey);
+//         if (whiteTheme) {
+//             body.style.backgroundColor = 'black';
+//             body.style.color = 'white';
+//             change_theme_btn.style.backgroundColor = 'white';
+//             change_theme_btn.style.color = 'black';
+//         }
+//         else {
+//             body.style.backgroundColor = 'white';
+//             body.style.color = 'black';
+//             change_theme_btn.style.backgroundColor = 'black';
+//             change_theme_btn.style.color = 'white';
+//         }
+//         whiteTheme = !whiteTheme;
+//     }
+// );
+
+
